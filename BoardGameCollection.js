191994de35e404098fbca19527db396056d41acc
Kelvin Beltre <kelvindbeltre@aol.com>
2019-01-18 22:01:46 -0500

    Updates

--- BoardGameCollection.js
+++ BoardGameCollection.js
@@ -1,93 +1,129 @@
 /*Variable initialization for board game images*/
 var gameCatan = { 
     name: "Catan", 
+    description: "In Catan (formerly The Settlers of Catan), players try to be the dominant force on the island of Catan by building settlements, cities, and roads. On each turn dice are rolled to determine what resources the island produces. Players collect these resources (cards)—wood, grain, brick, sheep, or stone—to build up their civilizations to get to 10 victory points and win the game.",
     theme: "Medieval", 
     playerCount: "2 - 4", 
-    imageURL: "https://images-na.ssl-images-amazon.com/images/I/81tWnmJQwBL._SX466_.jpg"
+    imageFullURL: "https://images-na.ssl-images-amazon.com/images/I/81tWnmJQwBL._SX466_.jpg",
+    imageIconURL: "https://cdn.anyfinder.eu/assets/7e1de0013179a6bd7eb5c37baf776c0b676828aeb7d8430623e743ea44b1e2e3?width=128",
+    greyedOut: 0
 };
 
 var gameClank = {
     name: "Clank", 
+    description: "Burgle your way to adventure in the deck-building board game Clank! Sneak into an angry dragon's mountain lair to steal precious artifacts. Delve deeper to find more valuable loot. Acquire cards for your deck and watch your thievish abilities grow.",
     theme: "Medieval", 
-    playerCount: "2 - 4", 
-    imageURL: "https://www.magicmadhouse.co.uk/images/clank-a-deck-building-adventure-p254691-239231_image.jpg"
+    playerCount: "2 - 4",
+    imageFullURL: "https://www.magicmadhouse.co.uk/images/clank-a-deck-building-adventure-p254691-239231_image.jpg",
+    imageIconURL: "http://static1.squarespace.com/static/54148cdae4b05a3412bfa19b/59adb114e4fcb54ed5c82942/59dd561e8fd4d260ef99cfd9/1507677747228/Clank+3D+box+square.jpg",
+    greyedOut: 0
 };
 
 var gameCoup = {
     name: "Coup", 
+    description: "You are head of a family in an Italian city-state, a city run by a weak and corrupt court. You need to manipulate, bluff and bribe your way to power. Your object is to destroy the influence of all the other families, forcing them into exile. Only one family will survive...",
     theme: "Sci-fi", 
     playerCount: "2 - 4", 
-    imageURL: "https://cdn-images-1.medium.com/max/1047/1*0f9z3uKRcFCK8S5gkU5hZg.jpeg"
+    imageFullURL: "https://cdn-images-1.medium.com/max/1047/1*0f9z3uKRcFCK8S5gkU5hZg.jpeg",
+    imageIconURL: "https://images-na.ssl-images-amazon.com/images/I/611YnRD6VxL._SY450_.jpg",
+    greyedOut: 0
 };
 
 var gameCoupRebellion = {
     name: "Coup: Rebellion", 
+    description: "In Coup: Rebellion G54 (G54), the last player with influence in the game wins, with influence being represented by face-down character cards in your playing area. Before each game players choose five character roles from a randomized deck. Characters have a unique variable power, and the deck is formed by three iterations of each role for fifteen cards total.",
     theme: "Sci-fi", 
     playerCount: "2 - 4", 
-    imageURL: "https://target.scene7.com/is/image/Target/GUEST_b7ba6202-b6f1-41fd-b849-3f113bd8c9e1?wid=488&hei=488&fmt=pjpeg"
+    imageFullURL: "https://target.scene7.com/is/image/Target/GUEST_b7ba6202-b6f1-41fd-b849-3f113bd8c9e1?wid=488&hei=488&fmt=pjpeg",
+    imageIconURL: "https://images-na.ssl-images-amazon.com/images/I/71kOOlAWKNL._SX466_.jpg",
+    greyedOut: 0
 };
 
 var gameDungeonsAndDragons = {
     name: "Dungeons & Dragons", 
+    description: "In D&D, characters embark upon imaginary adventures within a fantasy setting. A Dungeon Master serves as the game's referee and storyteller while maintaining the setting in which the adventures occur, and playing the role of the inhabitants. The characters form a party that interacts with the setting's inhabitants, and each other. Together they solve dilemmas, engage in battles, and gather treasure and knowledge.",
     theme: "Medieval", 
     playerCount: "2 - 4", 
-    imageURL: "https://images-na.ssl-images-amazon.com/images/I/81tWnmJQwBL._SX466_.jpg"
+    imageFullURL: "https://i2.wp.com/shaneplays.com/wp-content/uploads/2014/08/dd-fifth-edition-players-handbook-cover-art-782x1024.jpg",
+    imageIconURL: "https://images-na.ssl-images-amazon.com/images/I/81HHsBqXVaL.jpg",
+    greyedOut: 0
 };
 
 var gameKemet = {
     name: "Kemet", 
+    description: "In Kemet, players each deploy the troops of an Egyptian tribe and use the mystical powers of the gods of ancient Egypt – along with their powerful armies – to score points in glorious battles or through invasion of rich territories. A game is typically played to 8 or 10 victory points, which may be accrued through winning attacks, controlling temples, controlling fully-developed pyramids, sacrificing to the gods, and wielding particular magical powers.",
     theme: "Egyptian", 
     playerCount: "2 - 4", 
-    imageURL: "https://images-na.ssl-images-amazon.com/images/I/81tWnmJQwBL._SX466_.jpg"
+    imageFullURL: "https://cf.geekdo-images.com/imagepage/img/WtqfEYKrq2laGtDwP97bOb4ob2A=/fit-in/900x600/filters:no_upscale()/pic1431241.jpg",
+    imageIconURL: "https://b3h2.scene7.com/is/image/BedBathandBeyond/226682866536499p?$imagePLP$&wid=256&hei=256",
+    greyedOut: 0
 };
 
 var gameMageKnight = {
     name: "Mage Knight", 
+    description: "The Mage Knight board game puts you in control of one of four powerful Mage Knights as you explore (and conquer) a corner of the Mage Knight universe under the control of the Atlantean Empire. Build your army, fill your deck with powerful spells and actions, explore caves and dungeons, and eventually conquer powerful cities controlled by this once-great faction!",
     theme: "Medieval", 
     playerCount: "2 - 4", 
-    imageURL: "https://images-na.ssl-images-amazon.com/images/I/81tWnmJQwBL._tWnmJQwBL._SX466_.jpg"
+    imageFullURL: "https://cf.geekdo-images.com/imagepage/img/99jI1AXEfv3y1z0ZZmy2z8Ltxpc=/fit-in/900x600/filters:no_upscale()/pic1083380.jpg",
+    imageIconURL: "https://images-na.ssl-images-amazon.com/images/I/71zhrQilwNL._SX425_.jpg",
+    greyedOut: 0
 };
 
 var gameTicketToRide = {
     name: "Ticket to Ride", 
+    description: "Players collect cards of various types of train cars they then use to claim railway routes in North America. The longer the routes, the more points they earn. Additional points come to those who fulfill Destination Tickets – goal cards that connect distant cities; and to the player who builds the longest continuous route.",
     theme: "Abstract", 
     playerCount: "2 - 4", 
-    imageURL: "https://images-na.ssl-images-amazon.com/images/I/81tWnmJQwBL._SX466_.jpg"
+    imageFullURL: "https://cf.geekdo-images.com/imagepage/img/JrPAWdwNO6pAJGQ0snkUw7A0OcI=/fit-in/900x600/filters:no_upscale()/pic38668.jpg",
+    imageIconURL: "https://cdn7.bigcommerce.com/s-inbvhbfxpc/images/stencil/1280x1280/products/392/1715/days-of-wonder-ticket-to-ride-box-front__48569.1523561799.jpg?c=2?imbypass=on",
+    greyedOut: 0
 };
 
 var gameVast = {
-    name: "Vast", 
+    name: "Vast: The Crystal Caverns", 
+    description: "The dragon has been asleep for many long years. In that time, the cave under which it slumbered has changed greatly…Goblins and strange monsters have filled its gloomy depths and there are whispers that the cave itself has begun thinking, shifting, and growing evermore dangerous. Still, stories of peril rarely overshadow the rumors of riches. And riches there may be… For where a dragon slumbers, there also lies a fiercely guarded treasure.",
     theme: "Medieval", 
     playerCount: "2 - 4", 
-    imageURL: "https://images-na.ssl-images-amazon.com/images/I/81tWnmJQwBL._SX466_.jpg"
+    imageIconURL: "https://images-na.ssl-images-amazon.com/images/I/913T--jIztL._SX679_.jpg",
+    imageFullURL: "https://cf.geekdo-images.com/imagepage/img/y9dR2sZkOEM-B_2hxQFG8t2s65Q=/fit-in/900x600/filters:no_upscale()/pic2962289.jpg",
+    greyedOut: 0
 };
 
 //gamePlaceholder for empty spots in gameArray and sortArray
 var gamePlaceholder = {
-    name: "n/a", 
+    name: "", 
+    description: "n/a",
     theme: "n/a", 
     playerCount: "n/a", 
-    imageURL: "https://images-na.ssl-images-amazon.com/images/I/81tWnmJQwBL._SX466_.jpg"
+    imageIconURL: "n/a",
+    imageFullURL: "https://images-na.ssl-images-amazon.com/images/I/81tWnmJQwBL._SX466_.jpg",
+    greyedOut: 0
 };
 
 //Initialization of gameArray to hold all game vars and sortArray to display sorted order
 var gameArray = [gameCatan, gameClank, gameCoup, gameCoupRebellion, gameDungeonsAndDragons, gameKemet, gameMageKnight, gameTicketToRide, gameVast];
-var sortArray = [gameCatan, gameClank, gameCoup, gameCoupRebellion, gameDungeonsAndDragons, gameKemet, gameMageKnight, gameTicketToRide, gameVast, gamePlaceholder, gamePlaceholder, gamePlaceholder];
-populateTable();
+var sortArray = [gamePlaceholder, gamePlaceholder, gamePlaceholder, gamePlaceholder, gamePlaceholder, gamePlaceholder, gamePlaceholder, gamePlaceholder, gamePlaceholder];
+sortByNameAtoZ();
 
 //On click of tab, change background color/visible elements
 function changeTab(activeTab)
 {
     document.body.className = "body".concat(activeTab);
+    document.getElementById("databasePage").style.visibility = "hidden";
+    document.getElementById(((activeTab.slice(0, activeTab.indexOf("Tab")))).concat("Page")).style.visibility = "visible";
 }
 
 //On click of icon, display game information in gameTitle, gameDisplay, gameInfo
 function displayGameInfo(gameID)
 {
-    var gameVar = sortArray[gameID.slice(-3).replace(/\D/,"")];
+    var gameVar = sortArray[gameID.slice(-2).replace(/\D/,"")];
+    //Update all gameElements for passed non-gamePlaceholder parameter
     if (gameVar != gamePlaceholder){
-        document.getElementById("gameTitle").innerHTML = document.getElementById(gameID).innerText;
-       document.getElementById("gameDisplay").src = gameVar.imageURL;
+        document.getElementById("gameTitle").innerHTML = gameVar.name;
+        document.getElementById("gameDisplay").src = gameVar.imageFullURL;
+        document.getElementById("gameDescription").innerHTML = gameVar.description;
+        document.getElementById("gameTheme").innerHTML = "<b>Theme: </b>".concat(gameVar.theme);
+        document.getElementById("gamePlayers").innerHTML = "<b>Number of Players: </b>".concat(gameVar.playerCount);
     }
     return 0;
 }
@@ -96,13 +132,34 @@ function displayGameInfo(gameID)
 function populateTable()
 {
     for (i=0; i < sortArray.length; i++) {
-        document.getElementById("tableData".concat(i)).innerHTML = sortArray[i].name;
-    }
+        //Only read non-gamePlaceholder game objects
+        if (sortArray[i] != gamePlaceholder) {
+            document.getElementById("tableData".concat(i)).innerHTML = ("<img height='120px' width='120px' src='".concat(sortArray[i].imageIconURL)).concat("'>")};
+            
+            //Grey out image if display criteria isn't met
+            if (sortArray[i].greyedOut == 1) {
+                document.getElementById("tableData".concat(i)).style.webkitFilter = "grayscale(100%)";
+        }
+    };
+
+    //Instantly display first game in table
+    displayGameInfo("tableData0");
 }
 
-//Sort game objects by name into array, then call populateTable()
-function sortByName()
+//Sort game objects by name into array A to Z, then call populateTable()
+function sortByNameAtoZ()
 {
-    sortArray = gameArray.slice();
+    for (i=0; i < sortArray.length; i++) {
+        sortArray[i] = gameArray[i];
+    }
+    populateTable();
+}
+
+//Sort game objects by name into array Z to A
+function sortByNameAtoZ()
+{
+    for (i=0; i < sortArray.length; i++) {
+        sortArray[i] = gameArray[i];
+    }
     populateTable();
 }
